83d4c39fa2f236208f888638badbd46776f0f84f
Matt Harrington <querry43+github@underdogma.net>
2016-01-17 20:42:16 -0800

    freeing up some memory

--- mcp4131.lua
+++ mcp4131.lua
@@ -16,9 +16,9 @@ function MCP4131:new(o)
   return o
 end
 
-function MCP4131:increment() self:_write_byte(0x04) end
-function MCP4131:decrement() self:_write_byte(0x08) end
-function MCP4131:set(p) self:_write_word(0x00, p * self.max_pos / 100) end
+function MCP4131:increment() self:_write_word(0x04, 0x00) end
+function MCP4131:decrement() self:_write_word(0x08, 0x00) end
+function MCP4131:set(p) self:_write_word(0x00, p * self.max_pos / 100)end
 
 function MCP4131:_setup_once()
   if not MCP4131._was_setup then
@@ -27,27 +27,11 @@ function MCP4131:_setup_once()
   end
 end
 
-function MCP4131:_cs_low() gpio.write(self.cs, gpio.LOW) end
-function MCP4131:_cs_high() gpio.write(self.cs, gpio.HIGH) end
-
-function MCP4131:_write_byte(b)
-  b = bit.band(b, 0xff)
-  b = bit.bor(b, self.wiper)
-  
-  self:_cs_low()
-  spi.send(self._spi, b)
-  self:_cs_high()
-  tmr.delay(100)
-end
-
 function MCP4131:_write_word(b1, b2)
-  b1 = bit.band(b1, 0xff)
   b1 = bit.bor(b1, self.wiper)
-  b2 = bit.band(b2, 0xff)
-
-  self:_cs_low()
+  gpio.write(self.cs, gpio.LOW)
   spi.send(self._spi, b1)
   spi.send(self._spi, b2)
-  self:_cs_high()
+  gpio.write(self.cs, gpio.HIGH)
   tmr.delay(100)
 end
